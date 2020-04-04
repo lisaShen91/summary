814ae0352cb285ae373e169c5e124690ac0d01cc
申丽沙 <shenlisha@lisa-mac.local>
2020-04-04 23:04:59 +0800

    add full screen

---
+++ fullscreen.js
@@ -0,0 +1,24 @@
+
+// 进入全屏：
+function entryFullScreen() {
+    var docE = document.documentElement;
+    if (docE.requestFullScreen) {
+        docE.requestFullScreen();
+    } else if (docE.mozRequestFullScreen) {
+        docE.mozRequestFullScreen();
+    } else if (docE.webkitRequestFullScreen) {
+        docE.webkitRequestFullScreen();
+    }
+}
+
+// 退出全屏
+function exitFullScreen() {
+    var docE = document;
+    if (docE.exitFullscreen) {
+        docE.exitFullscreen();
+    } else if (docE.mozCancelFullScreen) {
+        docE.mozCancelFullScreen();
+    } else if (docE.webkitCancelFullScreen) {
+        docE.webkitCancelFullScreen();
+    }
+}
